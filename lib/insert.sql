bea34dcf16726adcb1e4701e1d7f6d5e1f6752ac
Kate Bennert <kate.bennert@gmail.com>
2023-01-28 14:47:16 -0500

    commit tree 8ba2e3bb6cf4a4bfbd5b8d8f0e26f4a17ac4d869

---
+++ lib/insert.sql
@@ -0,0 +1,67 @@
+INSERT INTO bears (
+    name,
+    age,
+    sex,
+    color,
+    temperament,
+    alive
+)
+VALUES
+    (
+        "Mr. Chocolate",
+        20,
+        "M",
+        "Brown",
+        "Cuddly",
+        0
+    ),
+     (
+        "Rowdy",
+        10,
+        "M",
+        "Light Brown",
+        "Rowdy",
+        1
+    ), (
+        "Tabitha",
+        6,
+        "F",
+        "Gold",
+        "Sassy",
+        1
+    ), (
+        "Sergeant Brown",
+        11,
+        "M",
+        "Brown",
+        "Uptight",
+        0
+    ), (
+        "Melissa",
+        13,
+        "F",
+        "Grayish Red",
+        "Annoying",
+        1
+    ), (
+        "Grinch",
+        2,
+        "M",
+        "Green",
+        "Standoffish",
+        1
+    ), (
+        "Wendy",
+        6,
+        "F",
+        "White",
+        "Hungry",
+        1
+    ), (
+        NULL,
+        5,
+        "M",
+        "Brown",
+        "Smart",
+        0
+    );
